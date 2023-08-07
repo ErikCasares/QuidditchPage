1a6d24538a92931fd21859bfdd9794bcaa1b2744
Erik Casares <erik.casaresps@gmail.com>
2023-08-07 18:23:33 -0300

    listo para editar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ItemListContainer from './Containers/ItemListContainer'
-import CheckListContainer from './Containers/CheckListContainer'
 import CartWidget from './components/CartWidget/CarWidget'
 import Navbar from './Containers/NavBar'
 
