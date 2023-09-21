c2aa58bd4bffde25d20d82353e63c447ab6704a1
Erik Casares <erik.casaresps@gmail.com>
2023-09-20 23:18:30 -0300

    holaasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import './App.css'
-import { CartComponentContext } from './CartContex/CartContex'
 import Router from './components/router/router'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { createContext } from 'react'
-import { useState } from 'react'
-import { db } from "./firebase/cliente"
-import {getDocs, collection, query, where, limit, getDoc, doc } from 'firebase/firestore'
-import { useEffect } from 'react'
+
+
 
 
 
 function App() {
-
-const productsRef = collection(db, "products")
-
-const getProducts = async () => {
-    const data = await getDocs(productsRef)
-    const dataFiltrada = data.docs.map((doc) => ( {...doc.data(), id: doc.id} ))
-    console.log(data)
-}
 
 return (
     <CartComponentContext>
